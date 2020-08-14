6527db59b801ce4dbb6f3dc10cdc5d6ac9a9dc4d
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-15 01:18:28 +0200

    init

---
+++ fileNameParser/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fileNameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
