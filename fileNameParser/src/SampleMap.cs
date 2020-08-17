9e405984fd006031a3cf99ff713290fdf7261f37
MordechaiHadad <33547558+MordechaiHadad@users.noreply.github.com>
2020-08-17 17:59:54 +0300

    Nice

--- fileNameParser/src/SampleMap.cs
+++ fileNameParser/src/SampleMap.cs
@@ -9,57 +9,57 @@ namespace fileNameParser.SampleMap
 
     class SampleMap
     {
-        public readonly string[] midiNotes = new string[128];
-        public readonly List<SampleGroup> groups = new List<SampleGroup>();
+        public string[] MidiNotes { get; } = new string[128];
+        public List<SampleGroup> Groups { get; } = new List<SampleGroup>();
         //public List<string> DynamicLevels = new List<string>();
         //public List<string> Articulations = new List<string>();
         //public List<string> RoundRobins = new List<string>();
         public SampleMap()
         {
             string[] notes = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-            for (int i = 0; i < midiNotes.Length; i++)
-                midiNotes[i] = $"{notes[i % 12]} {i / 12}";
+            for (int i = 0; i < MidiNotes.Length; i++)
+                MidiNotes[i] = $"{notes[i % 12]} {i / 12}";
         }
         public void AddRegion(Region region, string groupName)
         {
             var index = FindGroup(groupName);
             if (index != -1)
-                groups[index].regions.Add(region);
+                Groups[index].Regions.Add(region);
             else
             {
                 var g = new SampleGroup(groupName, region);
-                groups.Add(g);
+                Groups.Add(g);
             }
         }
         private int FindGroup(string groupName)
         {
-            for(var i=0; i<groups.Count;i++)
+            for(var i=0; i<Groups.Count;i++)
             {
-                if (groups[i].name == groupName)
+                if (Groups[i].name == groupName)
                     return i;
             }
             return -1;
         }
         public void SortRegions()
         {
-            foreach (var g in groups)
+            foreach (var g in Groups)
                 g.SortRegions();
         }
         public void StretchRegions(char mode)
         {
-            foreach (var g in groups)
+            foreach (var g in Groups)
             {
                 switch (mode)
                 {
                     case '1':
-                        g.regions[0].loKey = 0;
-                        for (var i = 1; i < g.regions.Count; i++)
-                            g.regions[i].loKey = g.regions[i - 1].midiNumber + 1;
+                        g.Regions[0].loKey = 0;
+                        for (var i = 1; i < g.Regions.Count; i++)
+                            g.Regions[i].loKey = g.Regions[i - 1].midiNumber + 1;
                         break;
                     case '2':
-                        g.regions[^1].hiKey = midiNotes.Length - 1;
-                        for (var i = 0; i < g.regions.Count-1; i++)
-                            g.regions[i].hiKey = g.regions[i + 1].midiNumber - 1;
+                        g.Regions[^1].hiKey = MidiNotes.Length - 1;
+                        for (var i = 0; i < g.Regions.Count-1; i++)
+                            g.Regions[i].hiKey = g.Regions[i + 1].midiNumber - 1;
                         break;
                     default:
                         break;
@@ -69,30 +69,30 @@ namespace fileNameParser.SampleMap
         public string Render(string defaultPath)
         {
             var result = $"<control>\ndefault_path={Path.GetFileNameWithoutExtension(defaultPath)}/\n\n";
-            groups.ForEach(group => result += group.Render());
+            Groups.ForEach(group => result += group.Render());
             return result;
         }
     }
 
     class SampleGroup
     {
-        public readonly List<Region> regions = new List<Region>();
+        public List<Region> Regions { get; } = new List<Region>();
         public readonly string name;
         public SampleGroup(string name) { this.name = name; }
         public SampleGroup(string name, Region region)
         {
             this.name = name;
-            regions.Add(region);
+            Regions.Add(region);
         }
 #if (DEBUG)
         public override string ToString() => name;
 #endif
-        public void SortRegions() { regions.Sort(); }
+        public void SortRegions() { Regions.Sort(); }
       
         public string Render()
         {
             var result = $"<group> //{name}\n";
-            regions.ForEach(r => result += r.Render().Indent() + "\n\n");
+            Regions.ForEach(r => result += r.Render().Indent() + "\n\n");
             return result;
         }
     }
@@ -107,7 +107,7 @@ namespace fileNameParser.SampleMap
         public Region(string file, string root)
         {
             var m = new SampleMap();
-            var midiNotes = m.midiNotes.ToList();
+            var midiNotes = m.MidiNotes.ToList();
             _file = file;
             _root = root;
             midiNumber = midiNotes.IndexOf(root);
