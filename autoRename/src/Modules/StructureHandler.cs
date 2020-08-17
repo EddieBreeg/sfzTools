bf208700408ed341b4acd442dbd1570917f29de1
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-17 22:51:21 +0200

    moved StructureHandler to a separate file

---
+++ autoRename/src/Modules/StructureHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace autoRename
+{
+    public class StructureHandler
+    {
+        public static string[][] SortByDirs(string rootPath, string extension = null)
+        {
+            var result = new List<string[]> { extension != null ? ByExtension(rootPath, extension) : Directory.GetFiles(rootPath) };
+            Directory.GetDirectories(rootPath).ToList().ForEach(d => result.AddRange(SortByDirs(d, extension)));
+
+            return result.ToArray();
+        }
+
+        public static string[] ByExtension(string path, string extension)
+        {
+            var files = new List<string>();
+            foreach (var file in Directory.GetFiles(path))
+            {
+                if (Path.GetExtension(file).ToLower() == extension.ToLower())
+                    files.Add(file);
+            }
+            return files.ToArray();
+        }
+    }
+}
--- autoRename/src/Program.cs
+++ autoRename/src/Program.cs
@@ -50,26 +50,4 @@ namespace autoRename
 #endif
         }
     }
-
-    public class StructureHandler
-    {
-        public static string[][] SortByDirs(string rootPath, string extension = null)
-        {
-            var result = new List<string[]> {extension != null? ByExtension(rootPath, extension): Directory.GetFiles(rootPath)};
-            Directory.GetDirectories(rootPath).ToList().ForEach(d => result.AddRange(SortByDirs(d, extension)));
-
-            return result.ToArray();
-        }
-
-        public static string[] ByExtension(string path, string extension)
-        {
-            var files = new List<string>();
-            foreach (var file in Directory.GetFiles(path))
-            {
-                if (Path.GetExtension(file).ToLower() == extension.ToLower())
-                    files.Add(file);
-            }
-            return files.ToArray();
-        }
-    }
 }
