fe7e359da18175682958388808f8f4f5a96a2daf
EddieBreeg <eddiebreeg0@protonmail.com>
2020-08-18 13:19:52 +0200

    chanced StructureHandler methods return types to List

--- autoRename/src/Modules/StructureHandler.cs
+++ autoRename/src/Modules/StructureHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -6,15 +7,15 @@ namespace autoRename
 {
     public class StructureHandler
     {
-        public static string[][] SortByDirs(string rootPath, string extension = null)
+        public static List<string[]> SortByDirs(string rootPath, string extension = null)
         {
-            var result = new List<string[]> { extension != null ? ByExtension(rootPath, extension) : Directory.GetFiles(rootPath) };
+            var result = new List<string[]> { extension != null ? ByExtension(rootPath, extension).ToArray() : Directory.GetFiles(rootPath) };
             Directory.GetDirectories(rootPath).ToList().ForEach(d => result.AddRange(SortByDirs(d, extension)));
 
-            return result.ToArray();
+            return result;
         }
 
-        public static string[] ByExtension(string path, string extension)
+        public static List<string> ByExtension(string path, string extension)
         {
             var files = new List<string>();
             foreach (var file in Directory.GetFiles(path))
@@ -22,7 +23,7 @@ namespace autoRename
                 if (Path.GetExtension(file).ToLower() == extension.ToLower())
                     files.Add(file);
             }
-            return files.ToArray();
+            return files;
         }
     }
 }
